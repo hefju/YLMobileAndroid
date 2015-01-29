b21736765ab9b7aadb724b89c9df4f720b14c29d
hefju <hefju@live.cn>
2015-01-29 23:02:06 +0800

    修改YLSystem类, 用户获取和设置当前登录用户, 获取wifi状态 修改YLWebService类, 实现从服务器取数

--- app/src/main/java/YLSystem/YLSystem.java
+++ app/src/main/java/YLSystem/YLSystem.java
@@ -1,24 +1,55 @@
 package YLSystem;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
 import TaskClass.User;
 
 /**
  * Created by asus on 2015/1/29.
  */
 public class YLSystem {
-    public static User user;//登录后的用户
 
-    public void SetUser(User _user)//设置用户
-    {
-        user=_user;
+    private static User user;
+    //登录后的用户
+    public static User getUser() {
+        return user;
     }
+
+    //设置用户
+    public static void setUser(User user) {
+        YLSystem.user = user;
+    }
+
+
+
+
     public String GetDeviceID()
     {
         return "NH008";//todo 等待添加方法从本机数据库取数
     }
 
-    public String GetISWIFI()
+    public String GetISWIFI(Context context)
     {
-        return "1";//todo 安卓如何获取wifi状态
+        return isWifiActive(context);
+    }
+
+    public static String isWifiActive(Context icontext){
+        Context context = icontext.getApplicationContext();
+        ConnectivityManager connectivity = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo[] info;
+        if (connectivity != null) {
+            info = connectivity.getAllNetworkInfo();
+            if (info != null) {
+                for (int i = 0; i < info.length; i++) {
+                    if (info[i].getTypeName().equals("WIFI") && info[i].isConnected()) {
+                        return "1";
+                      //  return true;
+                    }
+                }
+            }
+        }
+        return "0";// return false;
     }
 }
--- app/src/main/java/YLWebService/YLWebService.java
+++ app/src/main/java/YLWebService/YLWebService.java
@@ -1,9 +1,181 @@
 package YLWebService;
 
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONStringer;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import TaskClass.Site;
+import TaskClass.Task;
+import TaskClass.User;
+import YLSystem.YLSystem;
+
 /**
  * Created by asus on 2015/1/29.
  * 用于访问远程服务
  */
 public class YLWebService {
+  private static Map map = new HashMap();
+    public YLWebService()
+    {
+        map.put("Login","http://192.168.200.137:8055/YLMobileServiceAndroid.svc/Login");
+        map.put("TaskList","http://192.168.200.137:8055/YLMobileServiceAndroid.svc/GetTask");
+        map.put("SiteList","http://192.168.200.137:8055/YLMobileServiceAndroid.svc/GetTaskStie");
+    }
+
+    //获取网点列表
+    public List<Site> GetSiteList(String taskID){
+        try {
+            String url =map.get("SiteList").toString();
+            HttpPost post = new HttpPost(url);
+
+            //添加数值到User类
+            User s1 =YLSystem.getUser();
+
+            Gson gson = new Gson();
+            //设置POST请求中的参数
+            JSONObject p = new JSONObject();
+            p.put("taskID", taskID);//任务ID。
+            p.put("deviceID", s1.DeviceID);//手持机号
+            p.put("empid", s1.EmpID);//员工ID。
+            p.put("ISWIFI", s1.ISWIFI);//是否用WIFI
+
+            post.setEntity(new StringEntity(p.toString(), "UTF-8"));//将参数设置入POST请求
+            post.setHeader(HTTP.CONTENT_TYPE, "text/json");//设置为json格式。
+            HttpClient client = new DefaultHttpClient();
+            HttpResponse response = client.execute(post);
+            if (response.getStatusLine().getStatusCode() == 200) {
+                String content = EntityUtils.toString(response.getEntity());    //得到返回字符串
+                Log.d("WCF", content);//打印到logcat
+
+                List<Site> siteList = gson.fromJson(content, new TypeToken<List<Site>>() {
+                }.getType());
+            return siteList;
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    //获取任务列表
+    public List<Task>  DownloadTaskList(){
+        try {
+            String url = map.get("TaskList").toString();
+            HttpPost post = new HttpPost(url);
+            //添加数值到User类
+            User s1 =YLSystem.getUser();
+
+            Gson gson = new Gson();
+            //设置POST请求中的参数---------------返回List<YLTask>关于此员工对应的任务。
+            JSONObject p = new JSONObject();
+            p.put("empid", s1.EmpID);//员工ID。
+            p.put("deviceID", s1.DeviceID);//手持机号
+            p.put("ISWIFI", s1.ISWIFI);//是否用WIFI
+
+            post.setEntity(new StringEntity(p.toString(), "UTF-8"));//将参数设置入POST请求
+            post.setHeader(HTTP.CONTENT_TYPE, "text/json");//设置为json格式。
+            HttpClient client = new DefaultHttpClient();
+            HttpResponse response = client.execute(post);
+            if (response.getStatusLine().getStatusCode() == 200) {
+                String content = EntityUtils.toString(response.getEntity());    //得到返回字符串
+                Log.d("WCF", content);//打印到logcat
+
+                List<Task> taskList=new ArrayList<Task> ();
+                taskList = gson.fromJson(content, new TypeToken<List<Task>>() {
+                }.getType());
+
+                return taskList;
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    //用户登录
+    public boolean Login(String EmpNO,String UserName,String pwd,String DeviceID, String ISWIFI)
+    {
+        try {
+           // String url = "http://192.168.200.137:8055/YLMobileServiceAndroid.svc/Login";//网址
+            String url=map.get("Login").toString();
+            HttpPost post = new HttpPost(url);
+
+            //添加数值到User类
+            User s1 = new User();
+            s1.EmpNO =EmpNO;// "200097";
+            s1.Name = UserName;//"徐竞航";
+            s1.Pass = pwd;//"8c4d6ed1b2688b2373bcac4137fab1e6";
+            s1.DeviceID =DeviceID;// "NH008";
+            s1.ISWIFI =ISWIFI;// "1";
+
+            //设置POST请求中的参数-------返回EmpID员工ID，ServerReturn 服务器错误，1为没错误，Time 服务器时间。
+            JSONObject p = new JSONObject();
+            p.put("Lno", s1.EmpNO);//员工卡号。
+            p.put("Lpass", s1.Pass);//密码
+            p.put("DeviceID", s1.DeviceID);//手持机号
+            p.put("ISWIFI", s1.ISWIFI);//是否用WIFI
+            post.setEntity(new StringEntity(p.toString(), "UTF-8"));//将参数设置入POST请求
+            post.setHeader(HTTP.CONTENT_TYPE, "text/json");//设置为json格式。
+            HttpClient client = new DefaultHttpClient();
+            HttpResponse response = client.execute(post);
+            if (response.getStatusLine().getStatusCode() == 200) {
+                String content = EntityUtils.toString(response.getEntity());    //得到返回字符串
+                Log.d("WCF", content);//打印到logcat
+
+                //显示到列表里。
+                Gson gson = new Gson();
+                User s3=new User();
+                s3 = gson.fromJson(content, new TypeToken<User>() {
+                }.getType());
+
+                if(s3.ServerReturn.equals("1")) {
+                    YLSystem.setUser(s3);//登录成功后设置用户
+                    return true;
+                }
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
 
 }
