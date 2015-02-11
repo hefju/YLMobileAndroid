587541f74ac04342e03c775109e9d189628de3b6
hefju <hefju@qq.com>
2015-02-11 23:43:35 +0800

    修改数据库操作关键字

--- app/src/main/java/YLDataService/YLSQLHelper.java
+++ app/src/main/java/YLDataService/YLSQLHelper.java
@@ -36,10 +36,10 @@ public class YLSQLHelper extends SQLiteOpenHelper {
                 "ServerReturn varchar(50), ClientID varchar(50), ClientName varchar(50)," +
                 " ClientType varchar(50))");
 
-        db.execSQL("CREATE TABLE BaseEmp (Id INTEGER PRIMARY KEY identity NOT NULL, ServerReturn varchar(50)," +
+        db.execSQL("CREATE TABLE BaseEmp (Id INTEGER PRIMARY KEY autoincrement NOT NULL, ServerReturn varchar(50)," +
                 " EmpID varchar(50), EmpName varchar(50), EmpNo varchar(50), EmpHFNo varchar(50)," +
                 " EmpWorkState varchar(50), EmpJJNo varchar(50))");
-        db.execSQL("CREATE TABLE BaseSite (Id INTEGER PRIMARY KEY identity NOT NULL, ServerReturn varchar(50)," +
+        db.execSQL("CREATE TABLE BaseSite (Id INTEGER PRIMARY KEY autoincrement NOT NULL, ServerReturn varchar(50)," +
                 " SiteID varchar(50), SiteName varchar(50), SiteType varchar(50), ClientID varchar(50))");
 
     }
