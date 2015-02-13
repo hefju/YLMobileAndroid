565e2723cdbd941fcef6191cde49611fa638304f
tzchong <tzhcong@qq.com>
2015-02-13 15:22:31 +0800

    base类增加服务器时间和类型两个字段。

--- app/src/main/java/TaskClass/BaseEmp.java
+++ app/src/main/java/TaskClass/BaseEmp.java
@@ -1,5 +1,13 @@
 package TaskClass;
 
+import android.content.Context;
+import android.text.style.ForegroundColorSpan;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import YLDataService.BaseEmpDBSer;
+
 /**
  * Created by Administrator on 2015/2/11 0011.
  */
@@ -17,4 +25,22 @@ public class BaseEmp//员工类
     public String Mark ;//标记添加=1、修改=2、删除=3
     public String ServerTime;//服务器时间。
 
+    public void CacheBaseEmp(Context ctx,List<BaseEmp> lst){
+        ArrayList<BaseEmp> lstAdd=new ArrayList<>();
+        ArrayList<BaseEmp> lstUpdate=new ArrayList<>();
+        ArrayList<BaseEmp> lstDel=new ArrayList<>();
+        for (BaseEmp x : lst){
+            if(x.Id ==1){
+                lstAdd.add(x);
+            }else if(x.Id ==2){
+                lstUpdate.add(x);
+            }else if(x.Id ==3){
+                lstDel.add(x);
+            }
+        }
+        BaseEmpDBSer dbSer = new BaseEmpDBSer(ctx);
+        dbSer.DeleteBaseEmp(lstDel);
+        dbSer.UpdateBaseEmpByEmpID(lstUpdate);//update 不能根据ID来update而是根据EmpID来update的
+        dbSer.InsertBaseEmp(lstAdd);
+    }
 }
