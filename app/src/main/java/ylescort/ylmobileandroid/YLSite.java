110f8ba357bd016dccf155412af132eabae1790b
hefju <hefju@live.cn>
2015-02-28 09:22:45 +0800

    YLSite界面, 启动时候获取到用户选中YLTask.

--- app/src/main/java/ylescort/ylmobileandroid/YLSite.java
+++ app/src/main/java/ylescort/ylmobileandroid/YLSite.java
@@ -9,30 +9,39 @@ import android.widget.ListView;
 import java.util.List;
 
 import TaskClass.Site;
+import TaskClass.TasksManager;
+import TaskClass.YLTask;
 import YLDataService.SiteDBSer;
+import YLSystem.YLSystem;
 import adapter.YLSiteAdapter;
 
 
 public class YLSite extends ActionBarActivity {
 
     private ListView listView;
-
+    private TasksManager tasksManager = null;//任务管理类
+    private YLTask ylTask;//当前选中的任务
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ylsite);
 
-        Bundle bundle = this.getIntent().getExtras();
-        String taskid = bundle.getString("taskid");
+        tasksManager= YLSystem.getTasksManager();//获取任务管理类
+        ylTask=tasksManager.CurrentTask;//当前选中的任务
+
+
+//        Bundle bundle = this.getIntent().getExtras();
+//        String taskid = bundle.getString("taskid");
         listView = (ListView)findViewById(R.id.ylsite_lv_MainView);
-        LoadLocalData(taskid);
+        LoadLocalData();
 
     }
 
-    private void LoadLocalData(String taskid) {
-        SiteDBSer siteDBSer = new SiteDBSer(getApplicationContext());
-        List<Site> siteList = siteDBSer.GetSites("WHERE TaskID = '"+taskid+"'");
+    private void LoadLocalData() {
+//        SiteDBSer siteDBSer = new SiteDBSer(getApplicationContext());
+//        List<Site> siteList = siteDBSer.GetSites("WHERE TaskID = '"+taskid+"'");
+        List<Site> siteList =ylTask.lstSite;
         YLSiteAdapter ylSiteAdapter = new YLSiteAdapter(this,siteList,R.layout.activity_ylsiteitem);
         listView.setAdapter(ylSiteAdapter);
 
