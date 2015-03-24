a4041d066fce19c3c3028012d29752777f296d99
KimTree122 <839414748@qq.com>
2015-03-24 16:56:51 +0800

    增加当网络状态改变时User类立即更改iswifi状态功能.

--- app/src/main/java/ylescort/ylmobileandroid/YLNetWorkStateService.java
+++ app/src/main/java/ylescort/ylmobileandroid/YLNetWorkStateService.java
@@ -8,11 +8,18 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import TaskClass.User;
+import YLDataService.LocalSetting;
 import YLSystem.YLSystem;
 
 /**
@@ -30,28 +37,76 @@ public class YLNetWorkStateService extends Service {
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
             if (action.equals(ConnectivityManager.CONNECTIVITY_ACTION)) {
-//                Log.d(tag, "网络状态已经改变");
-//                Toast.makeText(context,"网络状态已经改变",Toast.LENGTH_SHORT).show();
                 connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
                 info = connectivityManager.getActiveNetworkInfo();
+
                 if(info != null && info.isAvailable()) {
+
                     String name = info.getTypeName();
                     if (name.equals("mobile")){
                         YLSystem.setNetWorkState("0");
+                        Log.d("ping", "0");
+
                     }else {
                         YLSystem.setNetWorkState("1");
+                        Log.d("ping", "1");
                     }
-//                    Log.d(tag, "当前网络名称：" + name);
-//                    Toast.makeText(context,"当前网络名称"+ name,Toast.LENGTH_SHORT).show();
+
+//                    String str = new NetPing().doInBackground();
+//                    if (str.equals("faild")){
+//                        Toast.makeText(context,"没有可用网络",Toast.LENGTH_SHORT).show();
+//                    }
                     //doSomething()
                 } else {
-//                    Log.d(tag, "没有可用网络");
                     Toast.makeText(context,"没有可用网络",Toast.LENGTH_SHORT).show();
                     //doSomething()
                 }
             }
         }
     };
+
+
+    public String Ping(String str){
+        String resault = "";
+        Process p;
+        try {
+            //ping -c 3 -w 100  中  ，-c 是指ping的次数 3是指ping 3次 ，-w 100  以秒为单位指定超时间隔，是指超时时间为100秒
+            p = Runtime.getRuntime().exec("ping -c 1 -w 50 " + str);
+            int status = p.waitFor();
+
+            InputStream input = p.getInputStream();
+            BufferedReader in = new BufferedReader(new InputStreamReader(input));
+            StringBuffer buffer = new StringBuffer();
+            String line = "";
+            while ((line = in.readLine()) != null){
+                buffer.append(line);
+            }
+            System.out.println("Return ============" + buffer.toString());
+
+            if (status == 0) {
+                resault = "success";
+            } else {
+                resault = "faild";
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return resault;
+    }
+
+    private class NetPing extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            String s = "";
+            s = Ping(LocalSetting.webintertest);
+            Log.d("ping", s);
+            return s;
+        }
+    }
 
 
     @Override
@@ -70,6 +125,7 @@ public class YLNetWorkStateService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        //new NetPing().doInBackground();
         return super.onStartCommand(intent, flags, startId);
     }
 
