758aa20f7ed9b09df2f521adc5e80a71ff0a49f1
KimTree122 <839414748@qq.com>
2015-04-21 17:48:19 +0800

    补录列表适配器

---
+++ app/src/main/java/YLAdapter/YLSiteAdapter.java
@@ -0,0 +1,84 @@
+package YLAdapter;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+import TaskClass.Site;
+import ylescort.ylmobileandroid.R;
+/**
+ * Created by Administrator on 2015/4/21.
+ */
+public class YLSiteAdapter extends BaseAdapter {
+
+    private List<Site> siteList;
+    private int resource;
+    private LayoutInflater inflater;
+    public YLSiteAdapter(Context context,List<Site> siteList,int resource){
+        this.siteList = siteList;
+        this.resource = resource;
+        inflater = (LayoutInflater)context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+    }
+
+
+    @Override
+    public int getCount() {
+        return siteList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return siteList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        TextView sitenum = null;
+        TextView sitename = null;
+        TextView sitestate = null;
+
+        if (convertView == null)
+        {
+            convertView = inflater.inflate(resource,null);
+            sitenum = (TextView) convertView.findViewById(R.id.ylsiteitem_Num);
+            sitename = (TextView) convertView.findViewById(R.id.ylsiteitem_Name);
+            sitestate = (TextView) convertView.findViewById(R.id.ylsiteitem_state);
+            ViewCache viewCache = new ViewCache();
+            viewCache.YLsitenumview = sitenum;
+            viewCache.YLsiteNameview = sitename;
+            viewCache.YLsitestateview = sitestate;
+            convertView.setTag(viewCache);
+        }else {
+            ViewCache viewCache = (ViewCache) convertView.getTag();
+            sitenum = viewCache.YLsitenumview;
+            sitename  = viewCache.YLsiteNameview;
+            sitestate = viewCache.YLsitestateview;
+        }
+
+        Site site = siteList.get(position);
+        sitenum.setText(site.getSiteType());
+        sitename.setText(site.getSiteName());
+        sitestate.setText(site.getStatus());
+
+        return convertView;
+    }
+
+
+    private final class ViewCache{
+        public TextView YLsitenumview;
+        public TextView YLsiteNameview;
+        public TextView YLsitestateview;
+    }
+
+
+}
